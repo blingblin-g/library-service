7be86a75b3eeb38ee38afd95855cd32804d81efe
blingblin-g <luvbliny@gmail.com>
2021-02-26 03:49:56 +0900

    [Add] linked all the files

--- app/__init__.py
+++ app/__init__.py
@@ -25,12 +25,19 @@ def create_app(test_config=None):
 	from . import db
 	db.init_app(app)
 
-	#from . import auth
-	#app.register_blueprint(auth.bp)
+	from . import auth
+	app.register_blueprint(auth.bp)
+	#app.add_url_rule("/auth/login", endpoint="login")
+	#app.add_url_rule("/auth/register", endpoint="register")
+
+	from . import profile
+	app.register_blueprint(profile.bp)
+	app.add_url_rule("/profile", endpoint="profile")
 	
 	from . import main
 	app.register_blueprint(main.bp)
 	app.add_url_rule("/", endpoint="index")
+
 
 	
 	return app
